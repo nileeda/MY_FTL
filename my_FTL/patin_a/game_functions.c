885cef06bfd13a39188f3b66c18d3a748891619a
patin_a <patin_a@b299136a-4b1d-48c7-a67f-3dc54731caf2>
2016-11-11 09:16:39 +0000

    corrections mineures

--- my_FTL/patin_a/game_functions.c
+++ my_FTL/patin_a/game_functions.c
@@ -5,7 +5,7 @@
 ** Login   <patin_a@etna-alternance.net>
 ** 
 ** Started on  Wed Nov  9 10:02:51 2016 PATIN Adeline
-** Last update Thu Nov 10 19:54:36 2016 PATIN Adeline
+** Last update Fri Nov 11 10:06:41 2016 PATIN Adeline
 */
 #include "ftl.h"
 #include <stdlib.h>
@@ -60,19 +60,18 @@ int	stat(t_ship *ship)
   my_putstr("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
   my_putstr("| Coque : ");
   my_put_nbr(ship->hull);
-  my_putstr("                 \n");
   my_putstr("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
   my_putstr("| Dommage : ");
   my_put_nbr(ship->weapon->damage);
-  my_putstr("               \n");
   my_putstr("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
   my_putstr("| Energy : ");
   my_put_nbr(ship->drive->energy);
-  my_putstr("                \n");
   my_putstr("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
   my_putstr("| Esquive : ");
   my_put_nbr(ship->nav_tools->evade);
-  my_putstr("               \n");
+  my_putstr("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
+  my_putstr("| Secteur : ");
+  my_put_nbr(ship->nav_tools->sector);
   my_putstr(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ \n");
   return (0);
 }
